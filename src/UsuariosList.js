7b708c4b0b0a86df1013ba8ea06fbac033f32bc3
erika <edbalarezo@sudamericano.edu.ec>
2022-01-31 21:40:50 -0500

    se agrega metodo actualizar

---
+++ src/UsuariosList.js
@@ -0,0 +1,15 @@
+function UsuariosList(props){
+    return(
+        <table>
+            <thead>
+            <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Status</th>
+            </tr>
+            </thead>
+        </table>
+    )
+
+}
